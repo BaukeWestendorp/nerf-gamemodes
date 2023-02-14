b0312fc078ffcd3dc10bc7ba14a4890932b8082e
BaukeWestendorp <baukewestendorp@gmail.com>
2023-02-14 21:29:48 +0100

    Fix iPhone padding

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,7 @@
 
 <svelte:head>
     <link rel="manifest" href="manifest.webmanifest" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
 </svelte:head>
 
 <slot>
